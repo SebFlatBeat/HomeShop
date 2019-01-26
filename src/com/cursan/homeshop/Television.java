bfa9b1cce411ade62636d1db8553430ccabfc73f
SebFlatBeat <sebastien.darre@gmail.com>
2019-01-26 23:22:52 +0100

    Correction des class Television et Fridge

--- src/com/cursan/homeshop/Television.java
+++ src/com/cursan/homeshop/Television.java
@@ -4,17 +4,19 @@ public class Television extends Product {
     private int size;
     private String slabType;
 
-    public Television(String name, String description, double price) {
+    public Television(String name, String description, double price, int size, String slabType) {
         super(name, description, price);
         this.size = size;
         this.slabType = slabType;
     }
 
     public int getSize() {
+
         return size;
     }
 
     public String getSlabType() {
+
         return slabType;
     }
 }
